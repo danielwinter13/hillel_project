a5e740a5a2b657d5e7119a174cb547a784db73ae
Danylo Zyma <danielwinter772@gmail.com>
2024-11-24 22:38:18 +0200

    HT 4

--- Java_introduction/src/main/java/org/example/Main.java
+++ Java_introduction/src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
+
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //squareNum (3);
+        //volumeCount(2.3, 4);
+        ascensionCount(2,3);
+    }
+    public static void squareNum(int a) {
+        System.out.println("Введіть ціле число:" + a);
+        System.out.println("Квадрат числа дорівнює "  + (a*a));
+    }
+    public static void volumeCount(double a, double b) {
+        System.out.println("Об'єм циліндру з радіусом " + a + " та висотою " + b + " дорівнює " + (a * b));
+    }
+    public static void ascensionCount(int a, int b) {
+        System.out.println("Введіть a:" + a + "\n" + "Введіть b:" + b + "\n" + "Результат " + a + "^" + b + " дорівнює " + (int)Math.pow(a,b));
     }
 }
