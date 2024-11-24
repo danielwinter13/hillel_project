9fff37f27e5852613f3b1975706ea83ccaeaa868
Danylo Zyma <danielwinter772@gmail.com>
2024-11-25 00:26:42 +0200

    Added are u adult? task

--- Java_introduction/src/main/java/org/example/Main.java
+++ Java_introduction/src/main/java/org/example/Main.java
@@ -3,19 +3,50 @@ package org.example;
 
 
 public class Main {
-    public static void main(String[] args) {
-        //squareNum (3);
-        //volumeCount(2.3, 4);
-        ascensionCount(2,3);
-    }
-    public static void squareNum(int a) {
-        System.out.println("Введіть ціле число:" + a);
-        System.out.println("Квадрат числа дорівнює "  + (a*a));
-    }
-    public static void volumeCount(double a, double b) {
-        System.out.println("Об'єм циліндру з радіусом " + a + " та висотою " + b + " дорівнює " + (a * b));
-    }
-    public static void ascensionCount(int a, int b) {
-        System.out.println("Введіть a:" + a + "\n" + "Введіть b:" + b + "\n" + "Результат " + a + "^" + b + " дорівнює " + (int)Math.pow(a,b));
-    }
-}
+        public static void main(String[] args) {
+            //System.out.println(sleepIn(false, false));
+            //System.out.println(sleepIn(true, false));
+            //System.out.println(sleepIn(false, true));
+            //System.out.println(sumDouble(5,5));
+//            System.out.println(makes10(9,10));
+//            System.out.println(makes10(9,9));
+//            System.out.println(makes10(1,9));
+//            System.out.println(posNeg(1, 1, false));
+//            System.out.println(posNeg(-1, 1, false));
+//            System.out.println(posNeg(-4, -5, true));
+            System.out.println(checkAge(18));
+            System.out.println(checkAge(0));
+            System.out.println(checkAge(99));
+        }
+//        public static int sumDouble(int a, int b) {
+//            if (a == b){
+//                return (a + b)*2;
+//            }else {
+//                return a + b;
+//            }
+//        }
+//        public static boolean makes10(int a, int b) {
+//            if ((a == 10 || b == 10) || ((a + b) == 10)){
+//                return true;
+//            }else {
+//                return false;
+//            }
+//        }
+//        public static boolean posNeg(int a, int b, boolean negative) {
+//            if ((a < 0 && b < 0) && negative == true){
+//                return true;
+//            }else if ((a < 0 && b < 0) && negative == false){
+//             return false;
+//            }else if ((a > 0 && b <0) || (a < 0 && b > 0)) {
+//                return false;
+//            }else{
+//                return false;
+//            }
+            public static String checkAge(int a) {
+                if ( a >= 18){
+                    return "Ви є дорослою людиною";
+                }else {
+                    return "Ви не є дорослою людиною";
+                }
+            }
+        }
