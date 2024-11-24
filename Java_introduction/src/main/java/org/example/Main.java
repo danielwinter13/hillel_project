d489c7ae0d1aee10def12e5872cdf586551cb743
Danylo Zyma <danielwinter772@gmail.com>
2024-11-25 00:57:45 +0200

    Number line task added

--- Java_introduction/src/main/java/org/example/Main.java
+++ Java_introduction/src/main/java/org/example/Main.java
@@ -4,15 +4,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPerfectSquare(25));
-        System.out.println(isPerfectSquare(20));
-        System.out.println(isPerfectSquare(9));
+        System.out.println(calculateSumUpToN(1));
+        System.out.println(calculateSumUpToN(3));
+        System.out.println(calculateSumUpToN(0));
     }
-    public static boolean isPerfectSquare(int a) {
-                if(Math.sqrt(a) % 1 == 0){
-                    return true;
-                }else {
-                    return false;
-                }
+
+    public static String calculateSumUpToN(int N) {
+        if (N > 0) {
+            int sum = 0;
+                for (int i = 0; i <= N; i++){
+                    sum += i;
             }
+                return "" + sum;
+        } else {
+            return "Хибні вхідні параметри";
+        }
+    }
 }
