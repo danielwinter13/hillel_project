57f3ae5043144574da8d72cea0c1d548f7c24c60
Danylo Zyma <danielwinter772@gmail.com>
2024-11-25 01:05:58 +0200

    Student's grade task added

--- Java_introduction/src/main/java/org/example/Main.java
+++ Java_introduction/src/main/java/org/example/Main.java
@@ -4,20 +4,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(calculateSumUpToN(1));
-        System.out.println(calculateSumUpToN(3));
-        System.out.println(calculateSumUpToN(0));
+        System.out.println(getGrade(5));
+        System.out.println(getGrade(1));
+        System.out.println(getGrade(-3));
     }
 
-    public static String calculateSumUpToN(int N) {
-        if (N > 0) {
-            int sum = 0;
-                for (int i = 0; i <= N; i++){
-                    sum += i;
-            }
-                return "" + sum;
-        } else {
-            return "Хибні вхідні параметри";
+    public static String getGrade(int A) {
+        switch (A) {
+            case 1:
+                return "Погано";
+            case 2:
+                return "Не дуже добре";
+            case 3:
+                return "Посередньо";
+            case 4:
+                return "Не погано";
+            case 5:
+                return "Відмінно";
+            default:
+                return "Неправильна оцінка";
         }
     }
 }
