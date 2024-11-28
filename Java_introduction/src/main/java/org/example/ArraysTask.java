ebce866ac018c83845c62706f06a2f962bad5f30
Danylo Zyma <danielwinter772@gmail.com>
2024-11-28 22:32:20 +0200

    added ReverseArray task

--- Java_introduction/src/main/java/org/example/ArraysTask.java
+++ Java_introduction/src/main/java/org/example/ArraysTask.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class ArraysTask {
     public static void main(String[] args) {
-        MiddleValue();
-        Sorting();
+//        MiddleValue();
+//        Sorting();
+        ReverseArray();
     }
     public static void MiddleValue() {
         int[] num = {2, 4, 5, 4, 5};
@@ -20,6 +21,11 @@ public class ArraysTask {
         Arrays.sort(num2);
         int i = num2.length - 1;
         System.out.println(num2[0] + "\n" + num2[i]);
-
+    }
+    public static void ReverseArray() {
+        int[] array = {1,5,7,8,9,1,2,5,76,67,12};
+        for (int i = array.length - 1; i >= 0; i--){
+            System.out.print(array[i] + " ");
+        }
     }
 }
