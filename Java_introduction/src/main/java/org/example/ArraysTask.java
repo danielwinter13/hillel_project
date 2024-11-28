7a00209dfbe06e8045a9d75dc0a728be1e9e72c9
Danylo Zyma <danielwinter772@gmail.com>
2024-11-28 22:14:10 +0200

    added Sorting task

--- Java_introduction/src/main/java/org/example/ArraysTask.java
+++ Java_introduction/src/main/java/org/example/ArraysTask.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class ArraysTask {
     public static void main(String[] args) {
         MiddleValue();
+        Sorting();
     }
     public static void MiddleValue() {
         int[] num = {2, 4, 5, 4, 5};
@@ -11,5 +14,12 @@ public class ArraysTask {
             sum += i;
         }
         System.out.println(sum);
+    }
+    public static void Sorting() {
+        int[] num2 = {1,5,7,8,9,1,2,5,76,67,12};
+        Arrays.sort(num2);
+        int i = num2.length - 1;
+        System.out.println(num2[0] + "\n" + num2[i]);
+
     }
 }
