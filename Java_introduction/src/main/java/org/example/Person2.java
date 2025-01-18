9f3fd1b01b281d3fb0d78bb5fdb24088ec820de1
Danylo <danielwinter772@gmail.com>
2025-01-18 09:57:09 +0200

    Second task done

--- Java_introduction/src/main/java/org/example/Person2.java
+++ Java_introduction/src/main/java/org/example/Person2.java
@@ -15,17 +15,30 @@ public class Person2 {
 //
 //        System.out.println(women.getName());//перевизначив методи абстрактного класа Персон для нащадків
 //        System.out.println(men.getName());//перевизначив методи абстрактного класа Персон для нащадків
-        String[] arraytask1_1 = {"a", "bb", "a", "bb"};
-        System.out.println(getLengthOfStrings(arraytask1_1));
-        String[] arraytask1_2 = {"this", "and", "that", "and"};
-        System.out.println(getLengthOfStrings(arraytask1_2));
-        String[] arraytask1_3 = {"code", "code", "code", "bug"};
-        System.out.println(getLengthOfStrings(arraytask1_3));
+//        String[] arraytask1_1 = {"a", "bb", "a", "bb"};
+//        System.out.println(getLengthOfStrings(arraytask1_1));
+//        String[] arraytask1_2 = {"this", "and", "that", "and"};
+//        System.out.println(getLengthOfStrings(arraytask1_2));
+//        String[] arraytask1_3 = {"code", "code", "code", "bug"};
+//        System.out.println(getLengthOfStrings(arraytask1_3));
+        String[] arraytask2_1 = {"code", "bug"};
+        System.out.println(createMap(arraytask2_1));
+        String[] arraytask2_2 = {"man", "moon", "main"};
+        System.out.println(createMap(arraytask2_2));
+        String[] arraytask2_3 = {"man", "moon", "good", "night"};
+        System.out.println(createMap(arraytask2_3));
     }
-    public static Map<String, Integer> getLengthOfStrings(String[] array){
-        Map<String, Integer> stringMap = new HashMap<>();
-        for (int i=0; i < array.length; i++){
-            stringMap.put(array[i],array[i].length());
+//    public static Map<String, Integer> getLengthOfStrings(String[] array){
+//        Map<String, Integer> stringMap = new HashMap<>();
+//        for (int i=0; i < array.length; i++){
+//            stringMap.put(array[i],array[i].length());
+//        }
+//        return stringMap;
+//    }
+    public static Map<String, String> createMap(String[] array){
+        Map<String, String> stringMap = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            stringMap.put(String.valueOf(array[i].charAt(0)), String.valueOf(array[i].charAt(array[i].length() - 1)));
         }
         return stringMap;
     }
