5fae5c3c666bb1a5a08f0d978235d22f95829e93
Danylo <danielwinter772@gmail.com>
2025-03-15 13:06:30 +0200

    15.03

--- Java_introduction/src/main/java/org/example/Maps.java
+++ Java_introduction/src/main/java/org/example/Maps.java
@@ -1,7 +1,9 @@
 package org.example;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class Maps {
     public static void main(String[] args){
@@ -29,6 +31,15 @@ public class Maps {
         System.out.println(buildResultString(new String[]{"a", "b", "a"}));
         System.out.println(buildResultString(new String[]{"a", "b", "a", "c", "a", "d", "a"}));
         System.out.println(buildResultString(new String[]{"a", "", "a"}));
+        int[] arraytask4_1 = {3, 1, 2, 2, 1, 2, 3, 3, 3};
+        System.out.println(findNumber(arraytask4_1));
+        int[] arraytask5_1 = {1, 2, 3, 4};
+        int[] arraytask5_2 = {3, 5, 6};
+        System.out.println(CrossArrays(arraytask5_1, arraytask5_2));
+        int[] arraytask6_1 = {1, 2, 3, 4};
+        int[] arraytask6_2 = {3, 5, 6};
+        System.out.println(AddingArrays(arraytask6_1, arraytask6_2));
+
 
     }
     public static Map<String, Integer> getLengthOfStrings(String[] array){
@@ -76,5 +87,49 @@ public class Maps {
                 break;
             }
         }return doubledChar;
+    }
+    public static int findNumber(int[] numbers){
+        int result = 0;
+        HashMap<Integer, Integer> numbersList = new HashMap<>();
+        for (int i:numbers){
+            numbersList.put(i , numbersList.getOrDefault(i,0) + 1);
+        }
+        int mostFrequent = numbers[0];
+        int maxCount = 0;
+
+        for (Map.Entry<Integer, Integer> entry : numbersList.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                mostFrequent = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+        return mostFrequent;
+    }
+    public static boolean CrossArrays(int[] numbersA, int[] numbersB){
+        LinkedHashSet<Integer> A = new LinkedHashSet<>();
+        LinkedHashSet<Integer> B = new LinkedHashSet<>();
+        for (int numA: numbersA){
+            A.add(numA);
+        }
+        for (int numB: numbersB){
+            B.add(numB);
+        }
+        A.retainAll(B);
+        if(A.isEmpty()) return false;
+        else return true;
+    }
+    public static TreeSet<Integer> AddingArrays(int[] numA, int[] numB){
+        TreeSet<Integer> setA = new TreeSet<>();
+        TreeSet<Integer> setB = new TreeSet<>();
+        for (int numberA: numA){
+            setA.add(numberA);
+        }
+        for (int numberB: numB){
+            setB.add(numberB);
+        }
+        System.out.println(setA);
+        System.out.println(setB);
+        setA.addAll(setB);
+        return setA;
     }
 }
